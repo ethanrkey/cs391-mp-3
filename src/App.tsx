be568f6a123cad95e42e409134586174d299c257
Ethan Key <ethanrkey23@gmail.com>
2025-03-07 19:53:08 -0500

    commented stuff to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import Header from './html-components/Header.tsx';
-import Nav from './html-components/Nav.tsx';
+// import Nav from './html-components/Nav.tsx';
 import MainSection from './html-components/MainSection.tsx';
 import Footer from './html-components/Footer.tsx';
 import './index.css';
